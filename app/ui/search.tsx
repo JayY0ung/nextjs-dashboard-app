e5298c0484505166c4ad67af64df6805a868ae06
Jie Yang <outstanding.yang@gmail.com>
2024-06-07 12:17:24 +0800

    chapter 10 - adding search.

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,31 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  // Get the search params and current pathname
+  const searchParams = useSearchParams(); // Only available in client-side components
+  const pathname = usePathname();
+
+  // Destructure the replace function from the router
+  const { replace } = useRouter();
+
+  // Debounce the search input to prevent too many requests
+  const handleSearch = useDebouncedCallback((term: string) => {
+    console.log(`Searching...${term}`);
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+
+    // Update the URL with the user's search data
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +34,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
